4a6d1f2be90ecc3089c2dcff7374668060356733
leonidvaidner <leonid.vaydner@yandex.ru>
2023-10-27 13:50:45 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Input, Checkbox, Slider, Radio } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App(){
+
+    const onChange = (e) => {
+        console.log(`checked = ${e.target.checked}`);
+    };
+
+    const buttons = [];
+    const inputs = [];
+    const checkbox = []
+    const slides = []
+
+    const radio = []
+
+    for (let i = 1; i <= 500; i++) {
+        buttons.push(<Button key={i} type="primary">Primary Button {i}</Button>);
+    }
+
+    for (let i = 1; i <= 500; i++) {
+        inputs.push(<Input placeholder="Basic usage" key={i}/>);
+    }
+
+    for (let i = 1; i <= 500; i++) {
+        checkbox.push(<Checkbox onChange={onChange} key={i}>Checkbox</Checkbox>);
+    }
+
+    for (let i = 1; i <= 500; i++) {
+        slides.push(<Slider defaultValue={30} disabled={false}/>);
+    }
+
+    for (let i = 1; i <= 500; i++) {
+        radio.push(<Radio key={i}>Radio</Radio>);
+    }
+
+
+    return(
+        <div>
+            {buttons}
+            {inputs}
+            {checkbox}
+            {radio}
+        </div>
+    )
 }
-
-export default App;
